7c15e83bf88018670b17853a657e191538510093
lakshmikanth-reddy-au3 <blkr1994@yahoo.com>
2021-03-07 13:02:08 +0530

    some modifications

--- src/components/content.js
+++ src/components/content.js
@@ -13,23 +13,23 @@ export default function Content() {
               <p>Where will I get the gift card?</p>
               <Grid container className="grid-right">
                 <Grid item xs={3} className="icn">{icons["whatsapp"]}</Grid>
-                <Grid item xs={9} className="grid-right-text" >You will recieve it on Whatsapp/SMS on your mobile</Grid>
+                <Grid item xs={9} className="grid-right-text" >You will recieve it on Whatsapp/SMS on your mobile.</Grid>
                 
               </Grid>
             </Grid>
             <Grid className="grid-item" item xs={12} md={6}>
-            <p>Where will I get the gift card?</p>
+            <p>When will i receive the gift card?</p>
               <Grid container className="grid-right">
                 <Grid item xs={3} className="icn">{icons["notes"]}</Grid>
-                <Grid item xs={9} className="grid-right-text" >You will recieve it on Whatsapp/SMS on your mobile</Grid>
+                <Grid item xs={9} className="grid-right-text" >With in 48 hours, when your Go Kwik order is delivered.</Grid>
                 
               </Grid>
             </Grid>
             <Grid className="grid-item" item xs={12} md={6}>
-            <p>Where will I get the gift card?</p>
+            <p>What if I cancel the order?</p>
               <Grid container className="grid-right">
                 <Grid item xs={3} className="icn">{icons["cancel"]}</Grid>
-                <Grid item xs={9} className="grid-right-text" >You will recieve it on Whatsapp/SMS on your mobile</Grid>
+                <Grid item xs={9} className="grid-right-text" >The voucher will not delivered if you cancel the order</Grid>
                 
               </Grid>
             </Grid>
